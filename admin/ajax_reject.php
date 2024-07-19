d44d5550ba96812f7cf23911822167162cf9855e
Dominicmaina77 <mainadominic628@gamil.com>
2024-07-19 14:49:46 +0300

    phpbasics2

--- admin/ajax_reject.php
+++
@@ -1,9 +0,0 @@
-<?php
-@include("connection.php");
-$id = $_POST['id'];
-
-$update ="UPDATE doctors SET status='Rejected' WHERE id='$id'";
-$result = mysqli_query($connection,$update);
-
-
-?>
--- patient/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['patient'])){
-    unset($_SESSION['patient']);
-    header("Location:../include/index.php");
-}
